4ef23fc8aa39d138fb66fe24cf2353612abffca4
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-11 16:21:28 +0300

    SelectChange

--- BindingToEmployeeList.xaml.cs
+++ BindingToEmployeeList.xaml.cs
@@ -19,12 +19,12 @@ namespace WpfApp
     /// </summary>
     public partial class BindingToEmployeeList : Window
     {
-        public Employee Emp { get; set; }
+        public List<Employee> Emps { get; set; }
         public BindingToEmployeeList()
         {
             InitializeComponent();
-            
-           //Emp= EmployeeList.Emps[1];
+
+            //Emp= EmployeeList.Emps[1];
 
             //ID.DataContext = Emp;
             //Name.DataContext = Emp;
@@ -32,14 +32,19 @@ namespace WpfApp
             //Jop.DataContext = Emp;
 
             //EmpPanal.DataContext = Emp;
+            EmpsCom.ItemsSource = EmployeeList.Emps;
         }
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             int x = int.Parse(index.Text);
-            Emp = EmployeeList.Emps[x];
-            EmpPanal.DataContext = Emp;
+            EmpPanal.DataContext =EmployeeList.Emps[x];
 
+        }
+
+        private void EmpsCom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EmpPanal.DataContext = (Employee)EmpsCom.SelectedItem;
         }
     }
 }
