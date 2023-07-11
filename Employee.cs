4db30ef62f2416cb764717daf82b2dde75215e85
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-11 14:19:03 +0300

    Add Employees Static Data

---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Jop { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ EmployeeList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public static class EmployeeList
+    {
+        public static List<Employee> Emps { get; set; }
+        static EmployeeList()
+        {
+            Emps.Add(new Employee() { Id = 1 , Name = "Maher", Age=27 , Jop="Enginner"});
+            Emps.Add(new Employee() { Id = 2 , Name = "Saber" , Age=23 , Jop="Enginner"});
+            Emps.Add(new Employee() { Id = 3 , Name = "Eman" , Age=29 , Jop="Teacher"});
+            Emps.Add(new Employee() { Id = 4 , Name = "Fatema" , Age=25 , Jop= "Teacher" });
+            Emps.Add(new Employee() { Id = 5 , Name = "Enas" , Age=20 , Jop="Nurse"});
+        }
+    }
+}
